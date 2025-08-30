a4faf7843030ca43c350c07d64ffff97c9c41b57
Alexandre_Reboucas <areboucasr@icloud.com>
2025-08-30 08:43:19 -0300

    Ultima aula

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// // const funcao = require ('./script');
+
+// // funcao
+
+
+// import { somar } from "./index.js";
+
+// somar (2, 3);
+
+
+const fs = require ('fs');
---
+++ index.js
@@ -0,0 +1,14 @@
+// // Módulos
+
+// function somar (n1, n2) {
+//     console.log (n1 + n2)
+// };
+
+
+// module.exports = somar
+
+
+
+export function somar(n1, n2) {
+    console.log(n1 + n2)
+}
